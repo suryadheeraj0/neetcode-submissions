795e9f933878fe913003e5389bc0fa5b1f2c9851
suryadheeraj0 <86139723+suryadheeraj0@users.noreply.github.com>
2026-08-17 12:46:46 +0530

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = {}
+
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+
+        buckets = [[] for _ in range(len(nums) + 1)]
+
+        for num, count in freq.items():
+            buckets[count].append(num)
+
+        res = []
+
+        for count in range(len(buckets) - 1, 0, -1):
+            for num in buckets[count]:
+                res.append(num)
+
+                if len(res) == k:
+                    return res
+
+        return res
