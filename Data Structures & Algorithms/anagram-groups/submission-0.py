9c0bedad61c06fbb9f6cf340ef739799128c66a3
suryadheeraj0 <86139723+suryadheeraj0@users.noreply.github.com>
2026-08-17 11:52:06 +0530

    Update: anagram-groups - submission-0

--- Data Structures & Algorithms/anagram-groups/submission-0.py
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -1,10 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        d = {}
+        d = defaultdict(list)
+        res = []
         for i in strs:
-            key = tuple(sorted(i))
-            if key in d:
-                d[key].append(i)
-            else:
-                d[key]=[i]
-        return list(d.values())
+            char_freq = [0]*26
+            for j in i:
+                char_freq[ord(j)-ord('a')]+=1
+            d[str(char_freq)].append(i)
+        for i in d:
+            res.append(d[i])
+        return res
+            
