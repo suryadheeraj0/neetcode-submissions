5b7aaac1dd1c91fee092726a056057fb28c0f3ae
suryadheeraj0 <86139723+suryadheeraj0@users.noreply.github.com>
2026-09-06 11:34:04 +0530

    Update: reorder-linked-list - submission-0

--- Data Structures & Algorithms/reorder-linked-list/submission-0.py
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.py
@@ -1,38 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-
-        if not head or not head.next:
-            return
-
-        # STEP 1: Find middle
         fast = head
         slow = head
-
-        while fast and fast.next:
+        curr = head
+        while fast and fast.next and fast.next.next:
             fast = fast.next.next
             slow = slow.next
-
-        # STEP 2: Reverse second half
+            curr = curr.next
+        slow = slow.next
         prev = None
-        curr = slow.next
-        slow.next = None
-
-        while curr:
-            nxt = curr.next
-            curr.next = prev
-            prev = curr
-            curr = nxt
-
-        # STEP 3: Merge
-        first = head
-        second = prev
-
-        while second:
-            temp1 = first.next
-            temp2 = second.next
-
-            first.next = second
-            second.next = temp1
-
-            first = temp1
-            second = temp2
+        while slow:
+            next = slow.next
+            slow.next = prev
+            prev = slow
+            slow = next
+        curr.next = None
+        first_node = head
+        second_node = prev
+        flag = True
+        while first_node and second_node:
+            first_next = first_node.next
+            second_next = second_node.next
+            
+            first_node.next = second_node
+            second_node.next = first_next
+            
+            first_node = first_next
+            second_node = second_next
